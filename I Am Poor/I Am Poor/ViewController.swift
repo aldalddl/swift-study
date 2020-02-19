b12db707ff32c31631784978f63fe563041dda82
Minji Kang <aldalddl2007@gmail.com>
2020-02-19 15:55:59 +0900

    I Am Poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by MinJi Kang on 13/02/2020.
+//  Copyright © 2020 MinJi Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
